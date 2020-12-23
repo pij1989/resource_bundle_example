0001cc70a164be74eb04ddd60a7187cd96862139
dimon <dimon.pij@mail.ru>
2020-12-23 22:56:40 +0300

    done tasks

---
+++ src/com/pozharsky/dmitri/HamletMain.java
@@ -0,0 +1,34 @@
+package com.pozharsky.dmitri;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class HamletMain {
+    public static void main(String[] args) {
+        System.out.println("1 - eng\n2 - bel\nany - default");
+        char i = 0;
+        try {
+            i = (char) System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String country = "";
+        String language = "";
+        switch (i) {
+            case '1':
+                country = "US";
+                language = "en";
+                break;
+            case '2':
+                country = "BY";
+                language = "be";
+        }
+        Locale current = new Locale(language, country);
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("property.text", current);
+        String s1 = resourceBundle.getString("str1");
+        System.out.println(s1);
+        String s2 = resourceBundle.getString("str2");
+        System.out.println(s2);
+    }
+}
